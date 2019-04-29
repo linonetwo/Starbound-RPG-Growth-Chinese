6f8cfd347803bd0a917dea5cd65213571b8db810
linonetwo <linonetwo012@gmail.com>
2019-04-29 18:03:58 +0800

    feat: generate patch

--- scripts/generateMissingPatch.js
+++ scripts/generateMissingPatch.js
@@ -1,19 +1,30 @@
 // @flow
-import { findAsync, readAsync, writeAsync, existsAsync, dirAsync } from 'fs-jetpack';
+import { readAsync, writeAsync, dirAsync } from 'fs-jetpack';
 import { dirname } from 'path';
 import { it, _ } from 'param.macro';
-import { keyPathInObject, sanitizeJSON } from './utils'
+import { assign } from 'lodash';
+
+import { keysNeedTranslation } from './constants';
+import { keyPathInObject } from './utils';
 
 async function parseReport() {
   const report: string[] = await readAsync('./report.log', 'json');
+  // 创建不存在的文件夹
+  const missingTranslationPath = report.filter(it.startsWith('翻译文件缺失')).map(it.replace('翻译文件缺失 ', ''));
   await Promise.all(
-    report
-      .filter(it.startsWith('翻译文件缺失'))
-      .map(it.replace('翻译文件缺失 ', ''))
+    missingTranslationPath
       .map(itt => `${dirname(itt)}`)
       // .map(itt => `translation/${dirname(itt)}`)
       .map(dirAsync(_)),
   );
+  // 创建 patch JSON
+  missingTranslationPath
+    .map(readAsync(it, 'json'))
+    .map(fileJSON => keyPathInObject(fileJSON, keysNeedTranslation))
+    .map(places => places.map(place => assign(place, { op: 'replace', source: place.value })))
+    .map(patchesForAFile => {
+      return writeAsync(`${patchesForAFile[0].path}`, patchesForAFile);
+    });
 }
 
 parseReport();
