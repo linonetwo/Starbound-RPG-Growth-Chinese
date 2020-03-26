d8df6a47b8c6a008f0af12eb3ac7a9c100cabc0f
linonetwo <linonetwo012@gmail.com>
2020-03-26 23:33:07 +0800

    fix: 修复颜色标记

--- scripts/tryTranslation.js
+++ scripts/tryTranslation.js
@@ -5,6 +5,21 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const translate = new BaiduTranslate(process.env.TRANSLATION_APP_ID, process.env.TRANSLATION_SECRET, 'zh', 'en');
+
+function fixMistranslation(text: string) {
+  return text
+    .replace(/\^?红色(：|:)/g, '^red;')
+    .replace(/\^?绿色(：|:)/g, '^green;')
+    .replace(/\^?白色(：|:)/g, '^white;')
+    .replace(/\^?黄色(：|:)/g, '^yellow;')
+    .replace(/\^?(橘|橙)色(：|:)/g, '^orange;')
+    .replace(/\^?灰色(：|:)/g, '^gray;')
+    .replace(/\^?蓝色(：|:)/g, '^blue;')
+    .replace(/\^?(品|洋)红色(：|:)/g, '^magenta;')
+    .replace(/\^?重置(：|:)/g, '^reset;')
+    .replace(/\^?暗影(：|:)/g, '^shadow;');
+}
+
 export default function tryTranslation(value: string | Object): Promise<string | Object> {
   if (typeof value !== 'string') return Promise.resolve(value);
   if (!value) return Promise.resolve('');
@@ -16,7 +31,7 @@ export default function tryTranslation(value: string | Object): Promise<string |
         .then(({ trans_result: result }) => {
           if (result && result.length > 0) {
             const [{ dst }] = result;
-            return dst;
+            return fixMistranslation(dst);
           }
           lastResult = result;
           retryCount = number;
