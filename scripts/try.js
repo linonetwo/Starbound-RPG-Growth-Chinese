6921faacfe4a5dfcf749036ef583b336f6c561a2
linonetwo <linonetwo012@gmail.com>
2020-03-27 01:41:38 +0800

    Update try.js

--- scripts/try.js
+++ scripts/try.js
@@ -1,7 +1,7 @@
 import tryTranslation from './tryTranslation';
 
 tryTranslation('Arena Battle : Shattered Stars').then(res => {
-  console.log(res);
+  console.log(JSON.stringify(res));
   // { from: 'en',
   //   to: 'zh',
   //   trans_result: [ { src: 'apple', dst: '苹果' } ]
